0053f844d7b4d65324a1a00faa5a6ec538431782
omid-sar <mr.omid.sardari@gmail.com>
2024-06-14 09:29:55 -0400

    define GPT class

--- src/GPT2/models/gpt2_model.py
+++ src/GPT2/models/gpt2_model.py
@@ -14,4 +14,42 @@ config = ConfigurationManager()
 
 @dataclass 
 class GPTconfig:
-    block_size : 
+    block_size : int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd : int = 768 
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super(CausalSelfAttention, self).__init__()
+        self.config = config
+
+
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super(Block, self).__init__()
+        self.config = config
+
+        self.ln1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        
+
+
+
+class GPT(nn.Module):
+
+    def __init(self, config):
+        super(GPT, self).__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd), 
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]), 
+            ln_f = nn.LayerNorm(config.n_embd)
+        )) 
+        self.lm_head = nn.Linear(config.vocab_size, config.n_embd, bias=False)
