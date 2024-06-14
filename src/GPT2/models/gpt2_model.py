3fb1b0f865692dff4f81ce14b7ea95ad63d9f0d4
omid-sar <mr.omid.sardari@gmail.com>
2024-06-14 14:15:35 -0400

    CausalSelfAttention class

--- src/GPT2/models/gpt2_model.py
+++ src/GPT2/models/gpt2_model.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import torch
+import math
 import torch.nn as nn
 from torch.nn import functional as F
 
@@ -14,7 +15,7 @@ config = ConfigurationManager()
 
 @dataclass 
 class GPTconfig:
-    block_size : int = 1024
+    block_size : int = 1024 # Sequence Length
     vocab_size: int = 50257
     n_layer: int = 12
     n_head: int = 12
@@ -26,10 +27,37 @@ class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
         super(CausalSelfAttention, self).__init__()
-        self.config = config
+        assert config.n_embd % config.n_head == 0, "n_embd is not divisble by n_head" 
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        
+     
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd,config.n_embd)
+        # NOT really a 'bias', more of a mask, but following HF naming though
+        self.register_buffer("bias", torch.tril(config.block_size, config.block_size)).view(1,1,config.block_size, config.block_size)
 
-        self.c_attn = nn.Linear()
-        self.c_proj = 
+    def forward(self, x):
+        # (Batch, Seq_len, embd_dim)
+        B, T, C = x.size()
+        d_k = self.n_head, C // self.n_head 
+        # we concatant Wq, Wk, Wv and multiply them with X (Batch, Seq_len, embd_dim)
+        # then slpit then q,k,v and then reshape them based on size of head to calculate MultiHead attention 
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        # (Batch, Seq_len, embd_dim) -> (Batch, Seq_len, n_head, d_k) -> (Batch, n_head, Seq_len, d_k)
+        q = q.view(B, T, self.n_head, d_k).transpose(1, 2)
+        k = k.view(B, T, self.n_head, d_k).transpose(1, 2)
+        v = v.view(B, T, self.n_head, d_k).transpose(1, 2)
+
+        att = q @ k.transpose(-2,-1) * (1.0/ math.sqrt(d_k))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0 , float('-inf')) # MASK
+        att = F.softmax(att, dim=1)
+        #(Batch, n_head, Seq_len, Seq_len) X (Batch, n_head, Seq_len, d_k) = (Batch, n_head, Seq_len, d_k)
+        y = att @ v 
+        # (Batch, n_head, Seq_len, d_k) -> 
+        
+
 
 
 
