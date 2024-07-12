493705b1dbdb7f7649989de37a3b325b55c70395
omid-sar <mr.omid.sardari@gmail.com>
2024-07-12 09:17:10 -0400

    added is_master_process function to prevent GPU  GPUs overwrite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from GPT2.logging import logger 
+from GPT2.logging import logger, is_master_process
 from GPT2.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from GPT2.pipeline.stage_02_data_transformation import DataTransformationTrainingPipeline
 from GPT2.pipeline.stage_03_model_verification import ModelVerificationTrainingPipeline
@@ -8,43 +8,56 @@ from GPT2.pipeline.stage_04_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
-   logger.info(f"\n\nx{'=' * 80}x \n\n>>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx{'=' * 80}x")
+    if is_master_process():
+        logger.info(f"\n\nx{'=' * 80}x \n\n>>>>>> stage {STAGE_NAME} started <<<<<<") 
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    if is_master_process():
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx{'=' * 80}x")
 except Exception as e:
+    if is_master_process():
         logger.exception(e)
-        raise e
+    raise e
 
 STAGE_NAME = "Data Transformation stage"
 try:
-    logger.info(f"\n\nx{'=' * 80}x \n\n>>>>>> stage {STAGE_NAME} started <<<<<<")
+    if is_master_process():
+        logger.info(f"\n\nx{'=' * 80}x \n\n>>>>>> stage {STAGE_NAME} started <<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
-    train_loader, val_loader = data_transformation.main(use_multiprocessing=True)#%%% should be False MPS
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx{'=' * 80}x")
+    train_loader, val_loader = data_transformation.main(use_multiprocessing=True)  # Change as needed
+    if is_master_process():
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx{'=' * 80}x")
 except Exception as e:
-    logger.exception(e)
-    raise e 
+    if is_master_process():
+        logger.exception(e)
+    raise e
 
 STAGE_NAME = "Model Validation stage"
 try:
-    logger.info(f"\n\nx{'=' * 80}x \n\n>>>>>> stage {STAGE_NAME} started <<<<<<")
-    model_verification_pipeline = ModelVerificationTrainingPipeline()
-    model, optimizer, raw_model = model_verification_pipeline.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx{'=' * 80}x")
+    if is_master_process():
+        logger.info(f"\n\nx{'=' * 80}x \n\n>>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_verification = ModelVerificationTrainingPipeline()
+    model, optimizer, raw_model = model_verification.main()
+    if is_master_process():
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx{'=' * 80}x")
 except Exception as e:
-    logger.exception(e)
-    raise e 
+    if is_master_process():
+        logger.exception(e)
+    raise e
 
 STAGE_NAME = "Model Training stage"
 try:
-    logger.info(f"\n\nx{'=' * 80}x \n\n>>>>>> stage {STAGE_NAME} started <<<<<<")
+    if is_master_process():
+        logger.info(f"\n\nx{'=' * 80}x \n\n>>>>>> stage {STAGE_NAME} started <<<<<<")
     model_training = ModelTrainingPipeline(train_loader, val_loader, model, optimizer, raw_model, use_compile=False)
     model_training.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx{'=' * 80}x")
+    if is_master_process():
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx{'=' * 80}x")
 except Exception as e:
-    logger.exception(e)
+    if is_master_process():
+        logger.exception(e)
     raise e 
+
 import sys; sys.exit(0)
 STAGE_NAME = "Model Evaluation stage"
 try:
--- src/GPT2/logging/__init__.py
+++ src/GPT2/logging/__init__.py
@@ -4,9 +4,11 @@ import torch
 import logging
 import torch.distributed as dist
 
+def is_master_process():
+    return (not dist.is_initialized()) or (dist.get_rank() == 0)
 
 device = "cuda" if torch.cuda.is_available() else("mps" if hasattr(torch.backends, "mps") and torch.backends.mps.is_available() else "cpu")
-is_master_process = (not dist.is_initialized()) or (dist.get_rank() == 0)
+
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 absolute_path = os.path.abspath(os.path.dirname(__file__))
@@ -17,7 +19,7 @@ os.makedirs(log_dir, exist_ok=True)
 
 # Configure the logging
 handlers = [logging.FileHandler(log_filepath)]
-if device in ["cpu", "mps"] or is_master_process:
+if device in ["cpu", "mps"] or is_master_process():
     handlers.append(logging.StreamHandler(sys.stdout))
 
 logging.basicConfig(
